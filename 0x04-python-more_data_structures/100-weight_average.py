ecf534fcfd6f6822ade84997bcca0559281f2f2f
Kdoxa <gloryola3@gmail.com>
2022-09-28 17:47:31 -0700

    more structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,18 +2,24 @@
 
 def weight_average(my_list=[]):
 
-    if my_list == []:
+    if not my_list:
 
         return 0
 
-    sum = 0
 
-    total = 0
 
-    for item in my_list:
+    num = 0
 
-        sum += (item[0] * item[1])
+    den = 0
 
-        total += item[1]
 
-        return sum / total
+
+    for tup in my_list:
+
+        num += tup[0] * tup[1]
+
+        den += tup[1]
+
+
+
+        return (num / den)
